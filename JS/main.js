14e92eb2190482e1ed2b14c9294079b19842ed91
SamirMamede <anuarsamir@gmail.com>
2021-11-16 10:16:22 -0300

    projeto consulta cep finalizado

---
+++ JS/main.js
@@ -0,0 +1,25 @@
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/"
+    console.log(url);
+    $.ajax({
+        url: url,
+        type: "get",
+        success: function(response){
+            console.log(response);
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#titulo_cep").html("CEP " + response.cep);
+
+            /*
+            $("#logradouro").html(response.logradouro);
+            document.getElementById("logradouro").innerHTML = response.logradouro;
+            document.getElementById("bairro").innerHTML = response.bairro;
+            document.getElementById("localidade").innerHTML = response.localidade;
+            document.getElementById("uf").innerHTML = response.uf;
+            */
+        }
+    })
+}
